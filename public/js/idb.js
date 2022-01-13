888fd23ee7df3ff44e0500285275a09db9cb10cd
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2022-01-13 15:46:50 -0600

    fix idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,58 +1,78 @@
+// create variable to hold db connection
 let db;
 
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open('web_budget_db', 1);
 
 request.onupgradeneeded = function(event) {
+
     const db = event.target.result;
-    db.createObjectStore('new_transaction', {autoIncrement: true});
-}
+
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+  };
 
 request.onsuccess = function(event) {
     db = event.target.result;
-    if(navigator.onLine) {
-        uploadTransaction();
+  
+    if (navigator.onLine) {
+      uploadTransaction();
     }
-};
+  };
+  
+  request.onerror = function(event) {
 
-function uploadTransaction() {
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-    const getAll = budgetObjectStore.getAll();
-    getAll.onsuccess = function() {
-        if(getAll.result.length > 0) {
-            fetch('/api/transaction', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
-            }) 
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if(serverResponse.message) {
-                        throw new Error(serverResponse)
-                    }
-                    const transaction = db.transaction(['new_transaction'], 'readwrite');
-                    const budgetObjectStore = transaction.objectStore('new_transaction');
-                    budgetObjectStore.clear();
-                    alert('transactions Stored');
-                })
-                .catch(err => console.log(err))
-        }
-    }
-}
-
-
-request.onerror = function(event) {
     console.log(event.target.errorCode);
-}
+  };
+
 
 function saveRecord(record) {
+
     const transaction = db.transaction(['new_transaction'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-    budgetObjectStore.add(record);
-}
+  
 
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
 
+    transactionObjectStore.add(record);
+  }
+
+  function uploadTransaction() {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+        fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(serverResponse => {
+            if (serverResponse.message) {
+                throw new Error(serverResponse);
+            }
+            // open one more transaction
+            const transaction = db.transaction(['new_transaction'], 'readwrite');
+            // access transaction object store
+            const transactionObjectStore = transaction.objectStore('new_transaction');
+            // clear all items in your store
+            transactionObjectStore.clear();
+
+            alert('All saved transactions have been submitted!');
+            })
+            .catch(err => {
+            console.log(err);
+            });
+        }
+    };
+  }
+
+  // listen for app coming back online
 window.addEventListener('online', uploadTransaction);
